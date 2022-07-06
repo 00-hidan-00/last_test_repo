daabe9b3a91470e907a69d181f881e419dc889fd
00-hidan-00 <david.taranov@gmail.com>
2022-07-06 13:58:36 +0200

    add back3

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
